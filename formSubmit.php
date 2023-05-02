5fcb381f508588480e1aabeae61515c5eea5755d
AbhishekTomar108 <abhishektomar9211@gmail.com>
2023-05-02 02:41:29 -0700

    modified for c-panel

--- formSubmit.php
+++ formSubmit.php
@@ -1,7 +1,10 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-        $conn = new mysqli('localhost', 'root', '', 'user');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+        header('Access-Control-Allow-Origin: *');
+        $conn = mysqli_connect('localhost', 'id19546599_bootcampuser', 'XDpp3#S=K6g1rE{h', 'id19546599_user');
 
         if(mysqli_connect_error()){
             echo mysqli_connect_error();
@@ -9,22 +12,28 @@ header('Access-Control-Allow-Origin: *');
         }
 
         else{
+            if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+           
            $name = $_POST['name'];
            $email = $_POST['email'];
            $mobile = $_POST['mobile'];
            $location = $_POST['location'];
+           $mode = $_POST['mode'];
 
-           $sql = "INSERT INTO userdetails(name,email,mobile,location) VALUES ('$name','$email','$mobile','$location')";
+     
+
+           $sql = "INSERT INTO FullStackUserDetails(name,email,mobile,location,mode) VALUES ('$name','$email','$mobile','$location','$mode')";
            $res = mysqli_query($conn,$sql);
 
            if($res){
-            echo 'data submitted!';
+            echo true;
            }
 
            else{
-            echo 'Error!';
+            echo false;
            }
 
-           $conn->close();
         }
+    }
+        $conn->close();
 ?>
